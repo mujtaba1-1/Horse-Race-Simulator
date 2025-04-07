e71f0d9f3410d77460a08326c41baf196b82002b
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-04-07 13:56:29 +0100

    Added Horse Class definition (Fields, Constructor and Methods)

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -1,6 +1,10 @@
 
 /**
- * Write a description of class Horse here.
+ * This class represents a horse within a race simulation
+ * Each horse has a name, symbol, confidence level and a tracker
+ * for the distance travelled. This also tracks whether the horse
+ * has fallen during the race. It implements all the methods needed
+ * to get and set the horse's attributes.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -8,8 +12,11 @@
 public class Horse
 {
     //Fields of class Horse
-    
-    
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
       
     //Constructor of class Horse
     /**
@@ -17,7 +24,11 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+       name = horseName;
+       symbol = horseSymbol;
+       confidence = horseConfidence;
+       distanceTravelled = 0;
+       hasFallen = false;
     }
     
     
@@ -25,52 +36,52 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
-        
+        hasFallen = true;
     }
     
     public double getConfidence()
     {
-        
+        return confidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return distanceTravelled;
     }
     
     public String getName()
     {
-        
+        return name;
     }
     
     public char getSymbol()
     {
-        
+        return symbol;
     }
     
     public void goBackToStart()
     {
-        
+        distanceTravelled = 0;
     }
     
     public boolean hasFallen()
     {
-        
+        return hasFallen;
     }
 
     public void moveForward()
     {
-        
+        distanceTravelled++;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        confidence = newConfidence;
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        symbol = newSymbol;
     }
     
 }
