2cca98dc9e1ce19dbd48676ea629276448ae37fa
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-04-12 14:05:55 +0100

    Main method complete

--- Part1/Main.java
+++ Part1/Main.java
@@ -8,15 +8,6 @@ class Main {
 
         Race race = new Race(10);
 
-        Horse horse1 = new Horse('A', "Alpha", 0.8);
-        race.addHorse(horse1, 1);
-        
-        Horse horse2 = new Horse('B', "Bravo", 0.6);
-        race.addHorse(horse2, 2);
-
-        Horse horse3 = new Horse('T', "Thunder", 0.5);
-        race.addHorse(horse3, 3);
-
         race.startRace();
     }
 }
