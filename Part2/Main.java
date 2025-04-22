e34d9a3f14049fd5e5bc4ef340d9707ab5204e5b
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-04-22 22:30:41 +0100

    Removed uncessary comments

--- Part2/Main.java
+++ Part2/Main.java
@@ -1,16 +1,6 @@
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 
 class Main {
     public static void main(String[] args) {
-        PrintStream printStream = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        System.setOut(printStream);
-
-        //Race race = new Race(10);
-
-        //race.startRace();
-
-        HorseRacingGUI gui = new HorseRacingGUI();
-
+        new HorseRacingGUI();
     }
 }
